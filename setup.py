fc5c301ce268c9b322ef057a2ac75a46b7925633
Johan Rydberg <johan.rydberg@gmail.com>
2011-08-30 19:07:06 +0200

    Packaging stuff.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(name='nesoi',
+      version='0.0.0',
+      description='a coordination and configuration manager',
+      author='Johan Rydberg',
+      author_email='johan.rydberg@gmail.com',
+      url='http://github.com/jrydberg/nesoi',
+      packages=find_packages() + ['twisted.plugins']
+)
